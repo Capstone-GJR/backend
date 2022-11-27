9303dbff1d96850890eabf85bbf0fa4801dd7ebe
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-26 20:38:20 -0600

    User class added fields created for un and pw

---
+++ src/main/java/com/capstone/backend/User.java
@@ -0,0 +1,29 @@
+package com.capstone.backend;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Username cannot be blank")
+    @NonNull
+    @Column(nullable = false)
+    String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @NonNull
+    @Column(nullable = false)
+    String password;
+
+
+}
