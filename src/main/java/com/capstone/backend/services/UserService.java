3d226f62d1db1228d63b580297692cf0b1a0d7e2
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-04 13:02:01 -0600

    Rollback changes to User "getUser" methods. - getUser(string email) is needed for Auth purposes. both find by id and find by username MUST remain.

--- src/main/java/com/capstone/backend/services/UserService.java
+++ src/main/java/com/capstone/backend/services/UserService.java
@@ -5,6 +5,8 @@ import com.capstone.backend.entity.User;
 public interface UserService {
     User getUser(Long id);
     User getUserProfile(Long id);
+//    NEED THE STRING GET USER FOR AUTH PURPOSES
+    User getUser(String email);
     User saveUser(User user);
     User registerUser(User user);
     void deleteUser(Long id);
--- src/main/java/com/capstone/backend/services/UserServiceImpl.java
+++ src/main/java/com/capstone/backend/services/UserServiceImpl.java
@@ -16,8 +16,8 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-//TODO: Each user upon registration should be assigned a "unassigned space" where items that are not sorted into components or spaces can be placed OR unassigned spaces get an id of 0?
-//TODO: combine save and register, no need for them to be separate methods
+    //TODO: Each user upon registration should be assigned a "unassigned space" where items that are not sorted into components or spaces can be placed OR unassigned spaces get an id of 0?
+//TODO: Combine register and save into one. No need for both.
     @Override
     public User registerUser(User user){
         user.setPassword((bCryptPasswordEncoder.encode(user.getPassword())));
@@ -35,11 +35,16 @@ public class UserServiceImpl implements UserService {
         userRepository.deleteById(id);
     }
 
-
     @Override
     public User getUser(Long id) {
         Optional<User> user = userRepository.findById(id);
         return unwrapUser(user, id);
+    }
+//STRING GET USER IS NEEDED FOR AUTH PURPOSED
+    @Override
+    public User getUser(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        return unwrapUser(user, email);
     }
 
 // Method Returns the User Object with password field filled as "RESTRICTED"
@@ -62,5 +67,10 @@ public class UserServiceImpl implements UserService {
     static User unwrapUser(Optional<User> entity, Long id) {
         if (entity.isPresent()) return entity.get();
         else throw new EntityNotFoundException(id, User.class);
+    }
+
+    static User unwrapUser(Optional<User> entity, String email) {
+        if (entity.isPresent()) return entity.get();
+        else throw new EntityNotFoundException(email, User.class);
     }
 }
