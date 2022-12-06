e216b6e0326c56b35079c97ce3928ece89e32bf0
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-06 15:01:31 -0600

    Item Controller Complete

--- src/main/java/com/capstone/backend/web/ItemController.java
+++ src/main/java/com/capstone/backend/web/ItemController.java
@@ -1,9 +1,14 @@
 package com.capstone.backend.web;
 
+import com.capstone.backend.entity.Item;
+import com.capstone.backend.services.ItemService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -11,6 +16,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/item")
 public class ItemController {
 
+    ItemService itemService;
 
+    @PostMapping("/add")
+    public ResponseEntity<Item> createItem(@Valid @RequestBody Item item) {
+        return new ResponseEntity<>((itemService.saveItem(item)), HttpStatus.CREATED);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getById(@PathVariable Long id) {
+        Item item = itemService.getItem(id);
+        return new ResponseEntity<>(item, HttpStatus.OK);
+    }
+
+    @GetMapping("/all/component{id}")
+    public ResponseEntity<List<Item>> getAllByComponentId (@PathVariable Long component_id) {
+        List<Item> allItemsInComponent = itemService.getAllItemsByComponent(component_id);
+        return new ResponseEntity<>(allItemsInComponent, HttpStatus.OK);
+    }
+
+    @GetMapping("/all/space{id}")
+    public ResponseEntity<List<Item>> getAllBySpaceId (@PathVariable Long space_id) {
+        List<Item> allItemsInSpace = itemService.getAllItemsByComponent(space_id);
+        return new ResponseEntity<>(allItemsInSpace, HttpStatus.OK);
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Item> editItem (@PathVariable(name = "id") Long id, @RequestBody Item item) {
+        Item editedItem = itemService.editItem(id, item);
+        return new ResponseEntity<>(editedItem, HttpStatus.OK);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Item> deleteItem (@PathVariable Long id) {
+        itemService.deleteItem(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
