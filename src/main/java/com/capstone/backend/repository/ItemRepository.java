91b1530c65a5200e25d81e27fabdf1e0ed79c0f2
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-06 14:53:40 -0600

    Item Classes added.

---
+++ src/main/java/com/capstone/backend/repository/ItemRepository.java
@@ -0,0 +1,16 @@
+package com.capstone.backend.repository;
+
+import com.capstone.backend.entity.Item;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ItemRepository extends CrudRepository<Item, Long> {
+    Optional<Item> findById(Long id);
+    List<Item> findBySpaceId(Long space_id);
+    List<Item> findByComponentId(Long component_id);
+    void deleteById(Long id);
+}
---
+++ src/main/java/com/capstone/backend/web/ItemController.java
@@ -0,0 +1,16 @@
+package com.capstone.backend.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/item")
+public class ItemController {
+
+
+
+}
