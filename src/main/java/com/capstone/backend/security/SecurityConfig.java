3f1114d7e1c0f10a2789be4e5855590c517f0f4e
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-17 12:41:01 -0600

    fix security config

--- src/main/java/com/capstone/backend/security/SecurityConfig.java
+++ src/main/java/com/capstone/backend/security/SecurityConfig.java
@@ -2,16 +2,16 @@ package com.capstone.backend.security;
 
 
 import com.capstone.backend.security.filter.AuthenticationFilter;
-import com.capstone.backend.security.filter.CorsFilter;
+import com.capstone.backend.security.filter.ExceptionHandlerFilter;
 import com.capstone.backend.security.filter.JWTAuthorizationFilter;
 import com.capstone.backend.security.manager.CustomAuthenticationManager;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import lombok.AllArgsConstructor;
 
 @Configuration
 @AllArgsConstructor
@@ -19,21 +19,17 @@ public class SecurityConfig {
     CustomAuthenticationManager customAuthenticationManager;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        CorsFilter corsFilter = new CorsFilter();
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
         authenticationFilter.setFilterProcessesUrl("/authenticate");
         http
-                .cors().disable()
                 .csrf().disable()
                 .authorizeRequests(authorizeRequests -> authorizeRequests
-                .antMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll() // Allows anyone to make post request on the path sign-up/register
-                .anyRequest().authenticated() )// requires that all requests (other than antMatcher specified requests) be authenticated
-//                .addFilterBefore()
-                .addFilterBefore(new CorsFilter(), AuthenticationFilter.class)
+                        .antMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll() // Allows anyone to make post request on the path sign-up/register
+                        .anyRequest().authenticated() )// requires that all requests (other than antMatcher specified requests) be authenticated
+                .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
                 .addFilter(authenticationFilter)
                 .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
         return http.build();
     }
 
