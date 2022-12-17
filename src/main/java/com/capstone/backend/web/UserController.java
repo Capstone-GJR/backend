5bb3b2cb4f233e2e5886e323625ec91415806fe0
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-16 18:09:24 -0600

    add .cors to SecurityConfig

--- src/main/java/com/capstone/backend/web/UserController.java
+++ src/main/java/com/capstone/backend/web/UserController.java
@@ -16,7 +16,7 @@ import java.security.Principal;
 
 @AllArgsConstructor
 @RestController
-@CrossOrigin(allowedHeaders = "*", origins = "http://localhost:3000, https://traqura.xyz")
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 @RequestMapping("/user")
 public class UserController {
 
