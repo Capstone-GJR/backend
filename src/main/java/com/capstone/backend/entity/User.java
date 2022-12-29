42b636d4ae55a2f35dc6b36f16b8515e37f8438c
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-29 14:11:44 -0600

    update email to unique constraint. update seeder2 file and seed remote DB

--- src/main/java/com/capstone/backend/entity/User.java
+++ src/main/java/com/capstone/backend/entity/User.java
@@ -23,7 +23,7 @@ public class User {
     @NotBlank(message = "email cannot be blank")
     @NonNull
     @Email
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @NotBlank(message = "Password cannot be blank")
