06a2e2ed22ee38264390b61e3add84c55389bdcf
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-29 15:45:46 -0600

    Update seeder file with new uploads

--- src/main/resources/db_seeder_2.sql
+++ src/main/resources/db_seeder_2.sql
@@ -7,14 +7,14 @@ VALUES ('TaraDactyl@dino.com', 'Tara', 'Dactyl', '$2a$10$wEc65/etNcCBKYLzlHTvvOI
        ('v-raptor@dino.com', 'Vicente', 'Raptor', '$2a$10$U5H/HJjxrRheOH784Qw1au6Cn0ZJtuDgfyrXh27DszVsTzG1bcIfC');
 
 INSERT INTO spaces (name, keywords, file_stack_url, color, user_id)
-VALUES ('living room', 'living room sofa family', 'https://cdn.filestackcontent.com/P21lCTrPQmK8sLbcIRmL', 'pink', 1),
-        ('Kitchen', 'kitchen', 'https://cdn.filestackcontent.com/gJPmLlkRRTRI4LF5lDSw', 'green', 1),
-        ('Garage', 'garage christmas tools pool', '"https://cdn.filestackcontent.com/Y1CH6J8vRnqYsKw0fAng"', 'blue', 1),
-        ('Office', 'office computer printer work', 'https://cdn.filestackcontent.com/lInb1BjLTe2uGgSsMtUZ', 'purple', 1),
-        ('Master Bedroom', 'bedding', 'https://cdn.filestackcontent.com/lInb1BjLTe2uGgSsMtUZ', 'purple', 1);
+VALUES ('living room', 'living room sofa family', 'https://cdn.filestackcontent.com/CoL42emPS7SOaId13J4X', 'pink', 1),
+       ('Kitchen', 'kitchen', 'https://cdn.filestackcontent.com/f7ImcSd4QViUi9UINJWQ', 'green', 1),
+       ('Garage', 'garage christmas tools pool', 'https://cdn.filestackcontent.com/Bpjxpfs7SDS7tgf6r0Lz', 'blue', 1),
+       ('Office', 'office computer printer work', 'https://cdn.filestackcontent.com/JirYxoAJScW4ZSHnb3oi', 'purple', 1),
+       ('Master Bedroom', 'bedding', 'https://cdn.filestackcontent.com/5QKgEpFdR0yQE2FpS6Qr', 'purple', 1);
 
 INSERT INTO totes (name, keywords, file_stack_url, color, space_id)
-VALUES ('TV Stand', 'movies, cds, books, board games', 'https://cdn.filestackcontent.com/e6kW3zccRlu1ZliKtF4F', '#8321a6', 1);
+VALUES ('TV Stand', 'movies, cds, books, board games', 'https://cdn.filestackcontent.com/nNJFYmhITZCnBi7FPgfT', '#8321a6', 1);
 
 INSERT INTO items (name, keywords, file_stack_url, color, tote_id, value)
-VALUES ('The Puffin in Bloom Collection Books', 'Little Women, Anne of Green Gables, A Little Prince, Heidi', 'https://cdn.filestackcontent.com/e6kW3zccRlu1ZliKtF4F', '#ee1bd2', 1, 36);
+VALUES ('The Puffin in Bloom Collection Books', 'Little Women, Anne of Green Gables, A Little Prince, Heidi', 'https://cdn.filestackcontent.com/XKLPTpeSvmaVCcKNy0tg', '#ee1bd2', 1, 36);
