67e53cee5c8b2970c1fdb8a8bc78a11c7cecbb17
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-05 09:56:00 -0600

    update seeder

---
+++ src/main/resources/seeder/spaces_seeder.sql
@@ -0,0 +1,6 @@
+INSERT INTO spaces (user_id, name,file_stack_url, keywords)
+VALUES (1, 'living room', 'https://cdn.filestackcontent.com/CoL42emPS7SOaId13J4X', 'sofa, family, tv'),
+       (1, 'Kitchen', 'https://cdn.filestackcontent.com/f7ImcSd4QViUi9UINJWQ', 'lighter'),
+       (1, 'Garage', 'https://cdn.filestackcontent.com/Bpjxpfs7SDS7tgf6r0Lz', 'garage, christmas, tools, pool'),
+       (1, 'Office', 'https://cdn.filestackcontent.com/JirYxoAJScW4ZSHnb3oi', 'office computer printer work'),
+       (1, 'Master Bedroom', 'https://cdn.filestackcontent.com/5QKgEpFdR0yQE2FpS6Qr', 'bedding');
---
+++ src/main/resources/seeder/user_seeder.sql
@@ -0,0 +1,7 @@
+# USE traqura_db;
+
+INSERT INTO users (email, first_name, last_name, password)
+VALUES ('taradactyl@dino.com', 'Tara', 'Dactyl', '$2a$10$wEc65/etNcCBKYLzlHTvvOILPkbi9DyFnIHaLRK8HO7vMArTZJIn2'),
+       ('terrancerex@dino.com', 'Terrance', 'Rex', '$2a$10$8.DVek/KxqJ9BfV2ZkNlEOjXqD7lrD5M0AhD0SXbPowSiA8YOmxK2'),
+       ('tri-sara@dino.com', 'Sara', 'Triceratops', '$2a$10$U5H/HJjxrRheOH784Qw1au6Cn0ZJtuDgfyrXh27DszVsTzG1bcIfC'),
+       ('v-raptor@dino.com', 'Vicente', 'Raptor', '$2a$10$U5H/HJjxrRheOH784Qw1au6Cn0ZJtuDgfyrXh27DszVsTzG1bcIfC');
